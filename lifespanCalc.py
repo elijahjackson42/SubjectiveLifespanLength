ba51f749e94aa6935b5c34c87299ad64e2384e2b
Eli <elijahjackson42@protonmail.com>
2021-11-25 16:14:13 -0600

    commit4

--- lifespanCalc.py
+++ lifespanCalc.py
@@ -1,7 +1,7 @@
 ##How long do you live?
-totalSpan=85
+totalSpan=24
 #Calculations will be based on how subjectively long this year felt
-compareYear=14
+compareYear=24
 ##What percentage of your total life will have subjectively passed
 percentageYear=35
 
@@ -14,9 +14,9 @@ def calcLifespan (totalSpan, compareYear):
     total=0.0
     totalAtPercentageYear=0.0
     i=1
-    while i < totalSpan:
+    while i < totalSpan+1:
         total+=1/i
-        print(1/i)
+        print('Year ',i,' feels like ',(1/i))
         if(i==percentageYear):
             totalAtPercentageYear=total
         i+=1
