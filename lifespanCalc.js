321ee8f33fdae45c430cac019e50360c4a33e2b0
Eli <elijahjackson42@protonmail.com>
2021-11-25 16:02:42 -0600

    commit3

--- lifespanCalc.js
+++ lifespanCalc.js
@@ -1,9 +1,6 @@
 /** 
-
 let totalSpan=125;
 let compareYear=18;
-
-
 const calcLifespan = (totalSpan, compareYear) =>{
     let subjectiveYear=1/compareYear
     let total=0;
@@ -14,7 +11,5 @@ const calcLifespan = (totalSpan, compareYear) =>{
     console.log(total);
     console.log(total/subjectiveYear);
 }
-
 calcLifespan(totalSpan,compareYear);
-
 */
